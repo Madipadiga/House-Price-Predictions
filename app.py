765ad7e15bbdf46fdb3a337cfda8685f7e774ba2
Madipadiga <manikantasai822@icloud.com>
2024-11-15 10:59:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
 from flask import Flask, render_template, request
 import pickle
 
+# Initialize Flask app
 app = Flask(_name_)
 
+# Load the model
 with open('model_pickle2', 'rb') as file:
     model = pickle.load(file)
 
-
-
+# Route for the home page
 @app.route("/")
 @app.route("/home")
-
 def home():
     return render_template("index.html")
 
-
-
-@app.route("/predict", methods = ["POST"])
+# Route to handle form submission and make predictions
+@app.route("/predict", methods=["POST"])
 def predict():
-    area =float(request.form['area'])
-    bedrooms =float(request.form['bedrooms'])
-    age =float(request.form['age'])
+    # Get form data
+    area = float(request.form['area'])
+    bedrooms = int(request.form['bedrooms'])
+    age = int(request.form['age'])
 
-    prediction = model.predict([[area,bedrooms,age]])
+    # Prepare data for prediction
+    prediction = model.predict([[area, bedrooms, age]])
 
-    return render_template("index.html", prediction = prediction[0])
+    # Render the HTML page with the prediction result
+    return render_template("index.html", prediction=prediction[0])
 
-if _name=="main_":
+if _name_ == "_main_":
     app.run(debug=True)
